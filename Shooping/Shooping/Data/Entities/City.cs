5c661cec1ab37ef47846f8e2bf37682fa6747a8b
Meta-Daryhen <Meta-Daryhen@DESKTOP-0PNRDG9>
2022-05-16 18:25:34 -0500

    Add administrators Change users Change password

--- Shooping/Shooping/Data/Entities/City.cs
+++ Shooping/Shooping/Data/Entities/City.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Shooping.Data.Entities
 {
@@ -11,6 +12,7 @@ namespace Shooping.Data.Entities
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
+        [JsonIgnore]
         public State State { get; set; }
 
     }
--- Shooping/Shooping/Helpers/IUserHelper.cs
+++ Shooping/Shooping/Helpers/IUserHelper.cs
@@ -9,7 +9,11 @@ namespace Shooping.Helpers
 
         Task<User> GetUserAsync(string email);
 
+        Task<User> GetUserAsync(Guid userId);
+
         Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task<User> AddUserAsync(AddUserViewModel model);
 
         Task CheckRoleAsync(string roleName);
 
@@ -20,6 +24,11 @@ namespace Shooping.Helpers
         Task<SignInResult> LoginAsync(LoginViewModel model);
 
         Task LogoutAsync();
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+      
 
     }
 }
